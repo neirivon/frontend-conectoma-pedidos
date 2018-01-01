3ea7c9c4ca92bf3d6c9c0b5389add0254fea683f
Neirivon Elias Cardoso <neirivon@gmail.com>
2018-01-01 15:05:15 -0200

    Mostrando dados e imagem do cliente para pagina de profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
-import { CategoriaService } from './../services/domain/categoria.service';
-import { ErrorInterceptorProvider } from './../interceptors/error-interceptor';
-import { AuthService } from './../services/auth.service';
-import { StorageService } from './../services/storage.service';
-
 import { MyApp } from './app.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { CategoriaService } from './../services/domain/categoria.service';
+import { ErrorInterceptorProvider } from './../interceptors/error-interceptor';
+import { AuthService } from './../services/auth.service';
+import { StorageService } from './../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
+
 
 @NgModule({
   declarations: [
@@ -33,7 +35,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     CategoriaService,
     ErrorInterceptorProvider,
     AuthService,
-    StorageService
+    StorageService,
+    ClienteService
   ]
 })
 export class AppModule {}
